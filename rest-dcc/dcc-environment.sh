1a7896346338fba10ee416da2299eaa9f711d0b7
Larne Pekowsky <lppekows@syr.edu>
2024-07-23 12:24:29 -0400

    Create common config directory

--- rest-dcc/dcc-environment.sh
+++
@@ -1,6 +0,0 @@
-export MARIADB_ROOT_PASSWORD=abcde
-export MARIADB_DATABASE=dcc
-export MARIADB_USER=dccuser1
-export MARIADB_PASSWORD=dccpw1
-export MARIADB_USER2=dccuser2
-export MARIADB_PASSWORD2=dccpw2
