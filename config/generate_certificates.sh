985485417266545ad4ae02443dcf592cda117fa1
Larne Pekowsky <lppekows@syr.edu>
2024-09-12 10:46:17 -0400

    Update generate_certificates.sh
    Removed hardcoded values

--- config/generate_certificates.sh
+++ config/generate_certificates.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-domain=surctest.org
+FQDN=$1
 
 # Create the root cert
-if [ ! -e dccCA.pem ]
+if [ ! -e surcCA.pem ]
 then
-    openssl genrsa -des3 -out dccCA.key 2048
-    openssl req -x509 -new -nodes -key dccCA.key -subj "/C=US/ST=CA/O=LIGO@caltech/CN=${FQDN}" -sha256 -days 1825 -out dccCA.pem
+    openssl genrsa -des3 -out surcCA.key 2048
+    openssl req -x509 -new -nodes -key surcCA.key -subj "/C=US/ST=CA/O=SU Researach Computing/CN=${FQDN}" -sha256 -days 1825 -out surcCA.pem
 fi
 
 # Create a server key
 openssl genrsa -out ${FQDN}.key 2048
 
 # and a signing request
-openssl req -new -key ${FQDN}.key -subj "/C=US/ST=CA/O=LIGO@caltech/CN=${FQDN}" -out ${FQDN}.csr
+openssl req -new -key ${FQDN}.key -subj "/C=US/ST=CA/O=SU Research Computing/CN=${FQDN}" -out ${FQDN}.csr
 
 cat > ${FQDN}.ext <<EOT
 authorityKeyIdentifier=keyid,issuer
@@ -26,7 +26,7 @@ DNS.1 = ${FQDN}
 EOT
 
 # Sign and get the cert
-openssl x509 -req -in ${FQDN}.csr -CA dccCA.pem -CAkey dccCA.key -CAcreateserial -out ${FQDN}.crt -days 825 -sha256 -extfile ${FQDN}.ext
+openssl x509 -req -in ${FQDN}.csr -CA surcCA.pem -CAkey surcCA.key -CAcreateserial -out ${FQDN}.crt -days 825 -sha256 -extfile ${FQDN}.ext
 
-rm dcc.popstar.party.csr dcc.popstar.party.ext dccCA.srl
+rm ${FQDN}.csr ${FQDN}.ext dccCA.srl
 
