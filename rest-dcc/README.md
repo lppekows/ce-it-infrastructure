85672e146ae71505c6ff66deeeffe5aacce35bf7
lppekows <lppekows@syr.edu>
2024-10-31 12:46:00 -0400

    Move from FastAPI+SQLAlchemy+Pydantic (which is broken) to a simpler implementation

--- rest-dcc/README.md
+++ rest-dcc/README.md
@@ -1,37 +1,23 @@
 # RESTful interface to the DCC DocDB tables in MariaDB
 
-This package implements a simple RESTful API to the DCC DocDB database using 
-[FastAPI](https://fastapi.tiangolo.com/) and [SQLAlchemy](https://www.sqlalchemy.org/)
-
+This package implements a simple RESTful API to the DCC DocDB database.
 
 Set up the environment:
 
-```
+```bash
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
 eval "$(${HOME}/miniconda3/bin/conda shell.bash hook)"
 
-conda create --name dcc
-conda create --name dcc conda-forge::sqlalchemy conda-forge::sqlacodegen conda-forge::fastapi conda-forge:mariadb conda-forge::mysqlclient conda-forge::uvicorn conda-forge::mysql-client=8.4.2
+conda create --name dcc conda-forge:mariadb conda-forge::mysqlclient conda-forge::mysql-client=8.4.2
 ```
 
 Note that version 8.4.2 is needed as later versions drop support for password authentication.
 
-To regenerate `tables.py` if needed (for example if the schema changes)
+Run the service:
 
-```
-# Adding +mariadbconnector would force communication over SSL, the MariaDB container would need
-# to be modified to support this.
-# sqlacodegen mariadb+mariadbconnector://root:abcde@127.0.0.1:3306/dcc
-
-# Parameters in the URL should come from the dcc-environment.sh.  Hardcoded here for development.
-sqlacodegen mariadb://root:abcde@127.0.0.1:3306/dcc > tables.py
-```
-
-Then to run the service
-
-```
-uvicorn main:app --reload
+```bash
+./RESTServer.py
 ```
 
 ## Generating an Entity Relationship Diagram
@@ -74,7 +60,7 @@ ALTER TABLE UsersGroup ADD CONSTRAINT FOREIGN KEY (GroupID) REFERENCES AuthorGro
 ALTER TABLE AuthorGroupList ADD CONSTRAINT FOREIGN KEY (AuthorGroupID) REFERENCES AuthorGroupDefinition (AuthorGroupID);
 ```
 
-NOTE: These changes may not be compatible with the DCC in production, there is no guarantee that the code adds
+NOTE: These changes will not be compatible with the DCC in production, there is no guarantee that the code adds
 entries to the database in a way that honors the foreign key relationships.  Either make these changes in a scratch
 database or delete and rebuild the database after generating the PDF.
 
---
+++ rest-dcc/RESTServer.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+import urllib.parse
+import mariadb
+
+# https://mariadb.com/resources/blog/how-to-connect-python-programs-to-mariadb/
+
+# Pydantic is broken to the point of unusability
+# https://github.com/Flagsmith/flagsmith/pull/3876
+# https://github.com/pydantic/pydantic/discussions/9343
+
+def format_value(v):
+    try:
+        return str(int(v))
+    except:
+        return f'"{v}"'
+
+class RESTHandler(BaseHTTPRequestHandler):
+    def send_result(self, status, data):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/json')
+        self.end_headers()
+
+        outs = json.dumps(data, separators=(',', ':'))
+        self.wfile.write(outs.encode('utf-8'))
+
+    def send_success(self, data):
+        self.send_result(200,data)
+
+    def send_failure(self, data):
+        self.send_result(500,data)
+
+    def parameters(self):
+        try:
+            length = int(self.headers['Content-Length'])
+            jdata  = self.rfile.read(length) 
+            data   = json.loads(jdata)
+        except:
+            data = None
+
+        url_info        = urllib.parse.urlparse(self.path)
+        path_components = url_info.path.split('/')
+        table_name      = len(path_components) > 1 and path_components[2] or None
+        qs              = urllib.parse.parse_qs(url_info.query)
+        
+        return table_name, data, qs
+
+
+    def do_DELETE(self):
+        table_name, data, f = self.parameters()
+        self.send_success({})
+
+
+def makeGET(conn):
+    def do_GET(self):
+        table_name,data,qs = self.parameters()
+        where = ""
+
+        if qs:
+            key = list(qs.keys())[0]
+            val = qs[key][0]
+            where = f"WHERE {key} = {format_value(val)}"
+
+        print(f"SELECT * FROM {table_name} {where}")
+        cur = conn.cursor(dictionary=True)
+        cur.execute(f"SELECT * FROM {table_name} {where}")
+        result = cur.fetchall()
+
+        print(f"json: {json.dumps(result,default=str)}")
+
+        self.send_success(json.dumps(result,default=str))
+
+    return do_GET
+
+def makePUT(conn):
+    def do_PUT(self):
+        table_name, data,f = self.parameters()
+        orderedKeys      = sorted([k for k in data.keys() if data[k]])
+        fields           = ','.join(orderedKeys)
+        values           = ','.join([format_value(data[k]) for k in orderedKeys])
+
+        try:
+            cur = conn.cursor()
+            print(f"INSERT INTO {table_name}({fields}) VALUES({values});")
+            cur.execute(f"INSERT INTO {table_name}({fields}) VALUES({values});")
+            self.send_success(data)
+        except Exception as e: 
+            print("Database error",e)
+            self.send_failure({'error':str(e)})
+
+    return do_PUT
+
+
+def run():
+    # TODO: move to a pool of connections
+    # https://mariadb-corporation.github.io/mariadb-connector-python/pool.html
+    try:
+        conn = mariadb.connect(
+            user="root",
+            password=os.environ["MARIADB_ROOT_PASSWORD"],
+            host=os.environ["FQDN"],
+            database=os.environ["MARIADB_DATABASE"],
+            port=3306
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+    server_class=HTTPServer
+    handler_class=RESTHandler
+
+    RESTHandler.do_GET  = makeGET(conn)
+    RESTHandler.do_PUT  = makePUT(conn)
+    RESTHandler.do_POST = makePUT(conn)
+
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+   
+
+if __name__ == '__main__':
+    run()
+
